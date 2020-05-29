5f000c9902eac28a5df0e9206580137188852312
Markus Hammerschmid <shiftedbit@Markuss-MacBook-Pro.local>
2020-05-29 10:04:34 +0200

    firebase analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireAnalyticsModule, ScreenTrackingService } from '@angular/fire/analytics';
+import { AngularFireModule } from '@angular/fire';
+
+const analytics = [
+  AngularFireModule.initializeApp(environment.firebase),
+  AngularFireAnalyticsModule,
+]
 
 @NgModule({
   declarations: [
@@ -10,9 +18,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    environment.production === true ? [...analytics] : []
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
